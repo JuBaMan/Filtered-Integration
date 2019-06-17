4029c50886762ddd575eafbc9e3f97d3b212a654
Andrei Barbu <dream.fade.culture@gmail.com>
2019-06-17 12:16:41 +0100

    Replaces client model with a server running on port 5999 of the docker container

--- server.go
+++ server.go
@@ -6,7 +6,7 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -14,30 +14,24 @@ func main() {
 	webhookURL := "https://hooks.slack.com/services/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"
 	jsonBody := new(bytes.Buffer)
 
-	// The client should be integrated with a microservice running on a docker machine.
-	client := &http.Client{}
-
 	// Add function to get the message from a server.
-	message := `"First message of the app."`
+	message := `"Second message of the app."`
 
 	// Add a function to format json.
 	jsonBody.WriteString(`{"text":`)
 	jsonBody.WriteString(message)
 	jsonBody.WriteString(`}`)
 
-	request, err := http.NewRequest("POST", webhookURL, jsonBody)
-	if err != nil {
-		panic(err)
-	}
-	response, err := client.Do(request)
-	if err != nil {
-		panic(err)
-	}
-	defer response.Body.Close()
+	http.HandleFunc("/send", func(w http.ResponseWriter, r *http.Request) {
+		response, err := http.Post(webhookURL, "application/json", jsonBody)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		defer response.Body.Close()
+		log.Println(response)
+	})
 
-	// Diagnosis.
-	fmt.Println("response Status:", response.Status)
-	fmt.Println("response Headers:", response.Header)
-	body, _ := ioutil.ReadAll(response.Body)
-	fmt.Println("response Body:", string(body))
+	fmt.Println("Starting server...")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
