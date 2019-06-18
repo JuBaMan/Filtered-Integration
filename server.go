08e765cd24ac44ead6e7a125a8a49db8ad34896c
Andrei Barbu <dream.fade.culture@gmail.com>
2019-06-18 16:53:45 +0100

    Fixes bug where jsonBody was emptied after the first handle call

--- server.go
+++ server.go
@@ -4,31 +4,32 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
 	webhookURL := "https://hooks.slack.com/services/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"
-	jsonBody := new(bytes.Buffer)
 
 	// Add function to get the message from a server.
 	message := `"Second message of the app."`
 
 	// Add a function to format json.
-	jsonBody.WriteString(`{"text":`)
-	jsonBody.WriteString(message)
-	jsonBody.WriteString(`}`)
+	jsonBody := `{"text": ` + message + `}`
 
 	http.HandleFunc("/send", func(w http.ResponseWriter, r *http.Request) {
-		response, err := http.Post(webhookURL, "application/json", jsonBody)
+		spew.Dump(jsonBody)
+		spew.Dump(webhookURL)
+
+		response, err := http.Post(webhookURL, "application/json", strings.NewReader(jsonBody))
 		if err != nil {
 			fmt.Println(err.Error())
 		}
 		defer response.Body.Close()
-		log.Println(response)
 	})
 
 	fmt.Println("Starting server...")
