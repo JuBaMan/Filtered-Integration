e7a21c5d76016d3917bc1d1af4037199792ad805
Andrei Barbu <dream.fade.culture@gmail.com>
2019-06-19 13:54:47 +0100

    Adds Handler for slash commands incoming on /asset

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 // Webhooks URL on Workspace: Development, Channel: slack-app:
 // https://hooks.slack.com/services/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
 
+// Generate new publicly accessible link to slash commands by running "ngrok http 192.168.99.100:5999" in a separate terminal window
+
 package main
 
 import (
@@ -25,6 +27,14 @@ func main() {
 		spew.Dump(jsonBody)
 		spew.Dump(webhookURL)
 
+		response, err := http.Post(webhookURL, "application/json", strings.NewReader(jsonBody))
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		defer response.Body.Close()
+	})
+
+	http.HandleFunc("/answer", func(w http.ResponseWriter, r *http.Request) {
 		response, err := http.Post(webhookURL, "application/json", strings.NewReader(jsonBody))
 		if err != nil {
 			fmt.Println(err.Error())
